6d0c19cae64105ff21f299232a5728b0473d0d44
Max Playle <maxplayle04@gmail.com>
2020-11-26 13:22:27 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MaxPlayle.ArgumentTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("This application was executed with the following arguments:");
+            foreach (string argument in args) {
+                Console.WriteLine("* " + argument);
+            }
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Press any key to exit this application!");
+            Console.ReadKey();
+        }
+    }
+}
